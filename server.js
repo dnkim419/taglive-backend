e7de7401bbc7b7305baf95c7c3c339c5ef77d2bf
DAIN KIM <dnkim419@gmail.com>
2023-06-14 21:20:20 +0900

    Init: Apollo Server를 Apollo Server Express로 전환

--- server.js
+++ server.js
@@ -1,19 +1,34 @@
 // must require dotenv before importing anything else
 require("dotenv").config();
-import { ApolloServer } from "apollo-server";
+import express from "express";
+import logger from "morgan";
+import { ApolloServer } from "apollo-server-express";
 import { typeDefs, resolvers } from "./schema";
 import { getUser } from "./users/users.utils";
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: async ({ req }) => {
-    return {
-      loggedInUser: await getUser(req.headers.token),
-    };
-  },
-});
+const PORT = process.env.PORT;
 
-server
-  .listen()
-  .then(() => console.log("Sever is running on http://localhost:4000/"));
+const startServer = async () => {
+  const apollo = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: async ({ req }) => {
+      return {
+        loggedInUser: await getUser(req.headers.token),
+      };
+    },
+  });
+
+  await apollo.start();
+
+  const app = express();
+  app.use(logger("tiny"));
+  apollo.applyMiddleware({ app });
+  app.use("/static", express.static("uploads"));
+
+  app.listen({ port: PORT }, () => {
+    console.log(`🚀Server is running on http://localhost:${PORT}/graphql ✅`);
+  });
+};
+
+startServer();
